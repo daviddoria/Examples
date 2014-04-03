e1ad9604c0202d61fce3d8c7a6d5911ea1e6136b
David Doria <daviddoria@gmail.com>
2014-04-03 10:32:04 -0400

    Update CurveFitting.cpp
    Correct example (the functor was incorrect) and use NumericalDiff which makes it much simpler.

--- c++/Eigen/LevenbergMarquardt/CurveFitting.cpp
+++ c++/Eigen/LevenbergMarquardt/CurveFitting.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
 
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 #include <unsupported/Eigen/NonLinearOptimization>
+//#include <unsupported/Eigen/LevenbergMarquardt>
 
 // LM minimize for the model y = a x + b
 typedef std::vector<Eigen::Vector2d,Eigen::aligned_allocator<Eigen::Vector2d> > Point2DVector;
 
 Point2DVector GeneratePoints();
 
-struct MyFunctor
+// Generic functor
+template<typename _Scalar, int NX = Eigen::Dynamic, int NY = Eigen::Dynamic>
+struct Functor
 {
-  int operator()(const Eigen::VectorXf &x, Eigen::VectorXf &fvec) const
+typedef _Scalar Scalar;
+enum {
+    InputsAtCompileTime = NX,
+    ValuesAtCompileTime = NY
+};
+typedef Eigen::Matrix<Scalar,InputsAtCompileTime,1> InputType;
+typedef Eigen::Matrix<Scalar,ValuesAtCompileTime,1> ValueType;
+typedef Eigen::Matrix<Scalar,ValuesAtCompileTime,InputsAtCompileTime> JacobianType;
+
+int m_inputs, m_values;
+
+Functor() : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime) {}
+Functor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
+
+int inputs() const { return m_inputs; }
+int values() const { return m_values; }
+
+};
+
+
+struct MyFunctor : Functor<double>
+{
+  int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const
   {
     // "a" in the model is x(0), and "b" is x(1)
     for(unsigned int i = 0; i < this->Points.size(); ++i)
       {
-      fvec(i) = x(0) * this->Points[i](0) + x(1);
-      }
-    return 0;
-  }
-
-  int df(const Eigen::VectorXf &x, Eigen::MatrixXf &fjac) const
-  {
-    // This problem is R^2 -> R^(fvec.rows()), so the dimension of the Jacobian is fvec.rows() x 2
-    
-    float epsilon = 1e-5;
-    Eigen::VectorXf epsilonVector(2);
-
-    Eigen::VectorXf f(this->Points.size());
-    operator()(epsilonVector, f);
-    
-    for(unsigned int parameter = 0; parameter < 2; parameter++)
-      {
-      epsilonVector = x;
-      epsilonVector(parameter) += epsilon;
-
-      Eigen::VectorXf fForward(2);
-      operator()(epsilonVector, fForward);
-
-      fjac.col(parameter) = (fForward - f)/epsilon;
+      fvec(i) = this->Points[i](1) - (x(0) * this->Points[i](0) + x(1));
       }
 
     return 0;
@@ -51,40 +54,42 @@ struct MyFunctor
   int values() const { return this->Points.size(); } // The number of observations
 };
 
-Point2DVector GeneratePoints()
+struct MyFunctorNumericalDiff : Eigen::NumericalDiff<MyFunctor> {};
+
+Point2DVector GeneratePoints(const unsigned int numberOfPoints)
 {
   Point2DVector points;
-  // Model y = 2*x + 5 with some noise
-  for(unsigned int i = 0; i < 50; ++i)
+  // Model y = 2*x + 5 with some noise (meaning that the resulting minimization should be about (2,5)
+  for(unsigned int i = 0; i < numberOfPoints; ++i)
     {
-    float x = static_cast<float>(i);
+    double x = static_cast<double>(i);
     Eigen::Vector2d point;
     point(0) = x;
-    point(1) = 2.0f * x + 5.0 + drand48()/10.0f;
+    point(1) = 2.0 * x + 5.0 + drand48()/10.0;
     points.push_back(point);
     }
 
   return points;
 }
 
-int main(int argc, char *argv[])
+int main(int , char *[])
 {
-  Point2DVector points = GeneratePoints();
-  
-  Eigen::VectorXf x(2);
-  x.fill(1.0f);
+  unsigned int numberOfPoints = 50;
+  Point2DVector points = GeneratePoints(numberOfPoints);
 
-  MyFunctor functor;
+  Eigen::VectorXd x(2);
+  x.fill(2.0f);
+
+  MyFunctorNumericalDiff functor;
   functor.Points = points;
-  Eigen::LevenbergMarquardt<MyFunctor, float> lm(functor);
+  Eigen::LevenbergMarquardt<MyFunctorNumericalDiff> lm(functor);
 
-  lm.parameters.ftol = 1e-6;
-  lm.parameters.xtol = 1e-6;
-  lm.parameters.maxfev = 1000; // Max iterations
+  Eigen::LevenbergMarquardtSpace::Status status = lm.minimize(x);
+  std::cout << "status: " << status << std::endl;
 
-  lm.minimize(x);
+  //std::cout << "info: " << lm.info() << std::endl;
 
-  std::cout << "x that minimizes the function: " << x << std::endl;
+  std::cout << "x that minimizes the function: " << std::endl << x << std::endl;
 
   return 0;
 }
