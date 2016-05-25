24109d0202c89cb79f7923cf5188220c31e96f60
David Doria <daviddoria@gmail.com>
2016-05-25 07:49:48 -0500

    Update SolvePNP.cxx

--- c++/OpenCV/SolvePNP/SolvePNP.cxx
+++ c++/OpenCV/SolvePNP/SolvePNP.cxx
@@ -1,3 +1,7 @@
+/**
+ * This program takes a set of corresponding 2D and 3D points and finds the transformation matrix 
+ * that best brings the 3D points to their corresponding 2D points.
+ */
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
